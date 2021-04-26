2f8351d59ff524ea8156dbe6baaf5630ab923d47
tpisfil <tpisfil13@gmail.com>
2021-04-26 16:09:09 -0400

    commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function upsideDown(){
+    alert("hello");
+}
+
+function printOrWhateva(){
+    window.print();
+}
+
+var count=0;
+function counterPlusOne(){
+    count += 1; 
+    console.log(count);
+}
